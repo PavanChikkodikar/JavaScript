1a55bd7ae1c1b929115ec9f29f21c6248f3f5365
PavanChikkodikar <pavanchikkodikar@gmail.com>
2024-02-27 08:42:39 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,16 @@
+// console.log("Pavan");
+// alert("Hi...");
+
+
+name = "Pavan" //string
+age = 50 //integer
+x = null; //special value (empty value)
+y = undefined; 
+ifFollow = false;
+
+
+console.log(name)
+console.log(age)
+console.log(x)
+console.log(y)
+console.log(ifFollow)
