c8bcf86da42b4aa1146d0158d0ba1751142f9146
PavanChikkodikar <pavanchikkodikar@gmail.com>
2024-02-29 11:51:22 +0530

    loops and strings

---
+++ loops&strings.js
@@ -0,0 +1,99 @@
+for(let i=1;i<=5;i++){
+    console.log("Hi How are u!");
+}
+console.log("Loop ended");
+
+//sum of 1 to 10
+let sum = 0;
+let n = 10;
+
+for (let i=1;i<=n;i++){
+    sum = sum + i;
+}
+console.log("Sum",sum)
+
+// for of loop - this is used for string and arrays
+let str = "Pavan";
+let size = 0;
+for(let i of str){
+    console.log(i);
+    size++;
+}
+console.log("size of str",size)
+
+// for of in - this is used for objects
+let stud = {
+    name : "Pavan",
+    age : 22,
+    cgpa : 8.2,
+    grade : "FCD"
+};
+
+for (let key in stud){
+    console.log("key",key,"value",stud[key])
+}
+
+
+
+// let gNum = 25;
+
+// let userInput = prompt("guess no");
+// while(userInput != gNum){
+//     userInput = prompt("u entered wrong  no, guess again");
+// }
+// console.log("Correct no")
+
+
+
+// strings
+let s = "Pavan";
+let a = s.length;
+console.log(a)
+console.log(s[2])
+
+//Template literals
+let q =  `this is template literals`
+console.log(q);
+
+
+let stud1 = {
+    name : "Pavan",
+    age1 : 22,
+};
+
+// to create strings by doing substiuation of placeholders is called as string interpolation
+let res = `My name is ${stud1.name} and my age is ${stud1.age1}`;
+console.log(res);
+
+
+// string methods
+// strings are immutables in JS
+
+let st = "   Pavan Chikkodikar   "
+let st2 = "Mudhol"
+st = st.toUpperCase();
+console.log(st);
+
+st = st.toLowerCase();
+console.log(st);
+
+st = st.trim(); //remove white spaces from above string
+console.log(st);
+
+st = st.slice(2,8);
+console.log(st);
+
+st = st.concat(st2);
+console.log(st);
+// or
+st = st + st2;
+console.log(st);
+
+st = st.replace("Mudhol", "MDL")
+console.log(st);
+
+st = st.replaceAll("Mudhol", "MDL")
+console.log(st);
+
+st = st.charAt(5)
+console.log(st);
--- operators&conditionalStatements.js
+++ operators&conditionalStatements.js
@@ -61,3 +61,58 @@ if (mode1 === "light"){
 }
 
 console.log(color1);
+
+let x = 7
+if (x%2 === 0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
+
+// else - if statement 
+
+let age = 90
+
+if (age < 18){
+    console.log("Junier")
+}else if(age >= 18 && age < 50){
+    console.log("Adult")
+}else if(age >= 50 && age <= 65){
+    console.log("Senior")
+}else{
+    console.log("Ajja/Ajji")
+}
+
+// ternary operator
+
+let s = 30
+
+let result  = s >= 18 ? "Adult" : "Not an Adult";
+console.log(result)
+// or
+let result1  = s >= 18 ? console.log("Adult") : console.log("Not an Adult");
+
+//switch
+
+const exp = "Mango";
+
+switch(exp){
+    case "Banana":
+        console.log("banana");
+        break;
+    case "Orange":
+        console.log("Orange");
+        break;
+    case "Mango":
+        console.log("Mangoooo");
+        break;
+    default:
+        console.log("No fruit found");
+}
+
+// to take user input (it is temprary)
+let d = prompt("Hello how are u");
+console.log(d);
+
+// how to take user input
+
