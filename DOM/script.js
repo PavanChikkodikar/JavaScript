1954b88a3615f8e7976eeb15eaebe3fa51e4401c
PavanChikkodikar <pavanchikkodikar@gmail.com>
2024-03-18 20:31:50 +0530

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,41 @@
-alert("Hi")
-console.log("Hi")
+//  the Document Object Model (DOM) is a programming interface for web documents. It represents the structure of a web page as a tree-like structure, where each node represents a part of the document, such as elements, attributes, and text. The DOM provides a way for programs to interact with web pages dynamically
+
+
+// null is an empty object in JS
+
+// //DOM Manipulation 3 ways to access the elements
+
+// let button = document.getElementById("button"); // this will return the value of heading
+// console.dir(button);
+
+
+// let headings = document.getElementsByClassName("heading")// return html collection (similar to Array)
+// console.dir(headings);
+// console.log(headings);
+
+
+// let parags = document.getElementsByTagName("para");
+// console.dir(parags);
+
+
+//Query selector - this will return node list
+
+
+let firstEle = document.querySelector(".heading");
+console.dir(firstEle);
+
+
+let allParaElements = document.querySelectorAll(".heading");
+console.dir(allParaElements);
+
+
+let button = document.querySelector("#button");
+console.dir(button);
+
+
+// Properties
+
+
+
+
+
