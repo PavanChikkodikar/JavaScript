a03d75fb8f3666c3b152e10ea00974f2cb15c282
PavanChikkodikar <pavanchikkodikar@gmail.com>
2024-03-19 14:49:53 +0530

    Events

--- DOM-2/script.js
+++ DOM-2/script.js
@@ -1,16 +1,36 @@
-let div = document.querySelector("div")
-console.log(div);
+// let div = document.querySelector("div")
+// console.log(div);
 
-let id = div.getAttribute("id");
-console.log(id);
+// let id = div.getAttribute("id");
+// console.log(id);
 
+// let para = document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+// let paraa = document.querySelector("p");
+// console.log(paraa.setAttribute("class","newClass"));
+
+// div.style.backgroundColor="pink"
+
+// insert elements
+let newBtn = document.createElement("button");
+newBtn.innerText ="click me";
+console.log(newBtn);
+
+let div = document.querySelector("div");
+div.append(newBtn);
+// div.prepend(newBtn);
+// div.before(newBtn);
+// div.after(newBtn);
+
+let heading = document.createElement("h1");
+heading.innerHTML = "<i>Hi I am learning JS</i>";
+
+document.querySelector("body").prepend(heading);
+
+
+// delete elements
 let para = document.querySelector("p");
-console.log(para.getAttribute("class"));
-
-let paraa = document.querySelector("p");
-console.log(paraa.setAttribute("class","newClass"));
-
-div.style.backgroundColor="pink"
-
+para.remove();
 
 
