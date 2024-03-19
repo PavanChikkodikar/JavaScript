45d77a2d51de2750024c3b64a83e80a5cd635962
PavanChikkodikar <pavanchikkodikar@gmail.com>
2024-03-19 12:26:17 +0530

    DOM

---
+++ DOM-2/script.js
@@ -0,0 +1,16 @@
+let div = document.querySelector("div")
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
+
+let paraa = document.querySelector("p");
+console.log(paraa.setAttribute("class","newClass"));
+
+div.style.backgroundColor="pink"
+
+
+
